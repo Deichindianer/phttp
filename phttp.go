b9a47d18eb47d6c1e0c56b66677b5a841ae54614
Tobias Germer <tobias.germer@tui.com>
2022-01-02 16:07:23 +0100

    fix implementation; add options

--- phttp.go
+++ phttp.go
@@ -1,4 +1,4 @@
-package pbhttp
+package phttp
 
 import (
 	"fmt"
@@ -10,72 +10,117 @@ import (
 	"golang.org/x/time/rate"
 )
 
+// DefaultBackOff is an opinionated backoff.ExponentialBackOff.
+var DefaultBackOff = backoff.ExponentialBackOff{
+	InitialInterval:     500 * time.Millisecond,
+	RandomizationFactor: 0.5,
+	Multiplier:          1.5,
+	MaxInterval:         5 * time.Second,
+	MaxElapsedTime:      30 * time.Second,
+	Stop:                backoff.Stop,
+	Clock:               backoff.SystemClock,
+}
+
+// DefaultRateLimiter is an opinionated rate.Limiter.
+var DefaultRateLimiter = rate.NewLimiter(rate.Limit(1), 1)
+
 type Client struct {
 	HTTPClient  *http.Client
 	RateLimiter *rate.Limiter
-	EBackoff    *backoff.ExponentialBackoff
+	Backoff     backoff.BackOff
 }
 
-type ClientConfig struct {
-	MaxRPS   int
-	Timeout  time.Duration
-	EBackoff *backoff.ExponentialBackoff
-}
+// Option is a function to alter the behaviour of a Client.
+type Option func(c *Client)
 
-func New(cfg ClientConfig) *Client {
-	if cfg.EBackoff != nil {
-		return &Client{
-			HTTPClient: http.Client{
-				Timeout: cfg.Timeout,
-			},
-			RateLimiter: rate.NewLimiter(rate.Limit(cfg.MaxRPS), MaxRPS),
-			EBackoff:    cfg.EBackoff,
-		}
-	}
-	return &Client{
-		HTTPClient: http.Client{
-			Timeout: cfg.Timeout,
-		},
-		RateLimiter: rate.NewLimiter(rate.Limit(cfg.MaxRPS), MaxRPS),
-		EBackoff: &backoff.ExponentialBackOff{
-			InitialInterval:     500 * time.Millisecond,
-			RandomizationFactor: 0.5,
-			Multiplier:          1.5,
-			MaxInterval:         5 * time.Second,
-			MaxElapsedTime:      30 * time.Second,
-			Stop:                backoff.Stop,
-			Clock:               backoff.SystemClock,
-		},
+// WithHttpClient configures the Client to use the given http.Client.
+func WithHttpClient(httpClient *http.Client) Option {
+	return func(c *Client) {
+		c.HTTPClient = httpClient
 	}
 }
 
+// WithRateLimiter configures the Client to use the given rate.Limiter.
+func WithRateLimiter(limiter *rate.Limiter) Option {
+	return func(c *Client) {
+		c.RateLimiter = limiter
+	}
+}
+
+// WithBackOff configures the Client to use the given backoff.BackOff.
+func WithBackOff(bo backoff.BackOff) Option {
+	return func(c *Client) {
+		c.Backoff = bo
+	}
+}
+
+// New creates a Client and accepts Options to configure it.
+func New(opts ...Option) *Client {
+	client := &Client{
+		HTTPClient:  http.DefaultClient,
+		RateLimiter: nil,
+		Backoff:     nil,
+	}
+
+	for _, opt := range opts {
+		opt(client)
+	}
+
+	return client
+}
+
+// Do is the interface for http.Client.Do.
 func (c *Client) Do(req *http.Request) (*http.Response, error) {
-	resp, err := backoff.Retry(do, c.EBackoff)
-	if err != nil {
-		return fmt.Errorf("exhausted all retries: %w", err)
+	if c.Backoff == nil {
+		return c.do(req)
 	}
+
+	var resp *http.Response
+
+	operation := func() error {
+		var err error
+
+		resp, err = c.do(req)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	err := backoff.Retry(operation, c.Backoff)
+	if err != nil {
+		return nil, fmt.Errorf("exhausted all retries: %w", err)
+	}
+
+	return resp, nil
 }
 
+// HTTPError exposes the http.Response while also giving some convenience for the http status code & response body.
 type HTTPError struct {
 	Code     int
 	Body     string
 	Response *http.Response
 }
 
-func (he *HTTPError) Error() string {
-	if he.Body != "" {
-		return fmt.Sprintf("failed HTTP call: %d: %s", he.Code, he.Body)
+// Error will print the http status code and optionally the http response body.
+func (e HTTPError) Error() string {
+	if e.Body != "" {
+		return fmt.Sprintf("failed HTTP call: %d: %s", e.Code, e.Body)
 	}
-	return fmt.Sprintf("failed HTTP call: %d", he.Code)
+
+	return fmt.Sprintf("failed HTTP call: %d", e.Code)
 }
 
 func (c *Client) do(req *http.Request) (*http.Response, error) {
-	err := c.RateLimiter.Wait(req.Context())
-	if err != nil {
-		return nil, err
+	if c.RateLimiter != nil {
+		err := c.RateLimiter.Wait(req.Context())
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	resp, err := c.Client.Do(req)
+	resp, err := c.HTTPClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
@@ -88,7 +133,8 @@ func (c *Client) do(req *http.Request) (*http.Response, error) {
 				Response: resp,
 			})
 		}
-		return nil, backoff.Permament(HTTPError{
+
+		return nil, backoff.Permanent(HTTPError{
 			Code:     resp.StatusCode,
 			Body:     string(bodyBytes),
 			Response: resp,
@@ -107,11 +153,5 @@ func (c *Client) do(req *http.Request) (*http.Response, error) {
 
 func readHTTPBody(bodyReader io.ReadCloser) ([]byte, error) {
 	defer bodyReader.Close()
-
-	bodyBytes, err := io.ReadAll(bodyReader)
-	if err != nil {
-		return nil, err
-	}
-
-	return bodyBytes, nil
+	return io.ReadAll(bodyReader)
 }
